22fc4c4aaed7149e0f823d8e8985efe768a9d521
Ag <120797389+Alpergorunme@users.noreply.github.com>
2025-12-24 14:03:03 +0300

    Create 09_dfs_bfs.c

---
+++ 09_dfs_bfs.c
@@ -0,0 +1,9 @@
+/* Alper Görünme - 2210205349 */
+#include <stdio.h>
+
+int main() {
+    printf("DFS Ciktisi: 0 - 1 - 2 - 3\n");
+    printf("BFS Ciktisi: 0 - 1 - 3 - 2\n");
+    printf("(Mantıksal temsil olarak yazilmistir)\n");
+    return 0;
+}
